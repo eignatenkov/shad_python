80217629e1a9c0e6adceef7125680963a28723f5
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2015-12-02 23:38:20 +0300

    add project

---
+++ homework3_2.py
@@ -0,0 +1,36 @@
+import json
+import random
+
+
+def weighted_choice(choices):
+    total = sum(choices.values())
+    r = random.uniform(0, total)
+    upto = 0
+    for key in choices:
+        if upto + choices[key] >= r:
+            return key
+        upto += choices[key]
+
+with open("test_single_stats.txt") as f:
+    single_stats = json.load(f)
+with open("test_double_stats.txt") as f:
+    double_stats = json.load(f)
+
+prev = '.'
+line = ''
+for i in range(10000):
+    if prev[-1] == '.':
+        next_word = random.choice(single_stats.keys()).title()
+        prev = next_word.lower()
+    elif len(prev.split(' ')) == 1:
+        next_word = weighted_choice(single_stats[prev])
+        prev = prev+' '+next_word
+    else:
+        next_word = weighted_choice(double_stats[prev])
+        prev = prev.split(' ')[-1] + ' ' + next_word
+    if next_word == '.':
+        line += '.'
+    else:
+        line += ' ' + next_word
+
+print line
