2ac47e056e077aed7e94a45eae0d89458f7d360f
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-09-29 19:45:19 +0200

    add model 1

--- bayes_1/ignatenkov.py
+++ bayes_1/ignatenkov.py
@@ -15,13 +15,12 @@ def pa(params, model=2):
     :return: p, b
     """
     size = params['amax'] - params['amin'] + 1
-    return np.arange(params['amin'], params['amax'] + 1), np.ones(size)*1./size
+    return np.ones(size)*1./size, np.arange(params['amin'], params['amax'] + 1)
 
 
 def pb(params, model=2):
     size = params['bmax'] - params['bmin'] + 1
-    return np.arange(params['bmin'], params['bmax'] + 1), np.ones(
-        size) * 1. / size
+    return np.ones(size)*1./size, np.arange(params['bmin'], params['bmax'] + 1)
 
 
 def pd_c(c, params, model=2):
@@ -31,14 +30,28 @@ def pd_c(c, params, model=2):
 
 
 def pc_ab(a, b, params, model=2):
-    lmbd = a*params['p1'] + b*params['p2']
-    dist = poisson(lmbd)
-    size = params['amax'] + params['bmax'] + 1
-    support = np.arange(size)
-    if isinstance(a, np.ndarray):
-        return dist.pmf(support.reshape(size, 1)), support
-    else:
-        return dist.pmf(support), support
+    total_size = params['amax'] + params['bmax'] + 1
+    if model == 1:
+        c1 = binom(a, params['p1'])
+        c2 = binom(b, params['p2'])
+
+        def vector_convolve(num):
+            return np.convolve(num, c2.pmf(np.arange(params['bmax']+1)))
+        total_size = params['amax'] + params['bmax'] + 1
+        dist = np.apply_along_axis(vector_convolve, 0,
+                                   c1.pmf(np.arange(params['amax']+1).reshape(params['amax']+1, 1)))
+        if isinstance(a, np.ndarray):
+            return dist, np.arange(total_size)
+        else:
+            return dist.reshape(total_size), np.arange(total_size)
+    elif model == 2:
+        lmbd = a*params['p1'] + b*params['p2']
+        dist = poisson(lmbd)
+        support = np.arange(total_size)
+        if isinstance(a, np.ndarray):
+            return dist.pmf(support.reshape(total_size, 1)), support
+        else:
+            return dist.pmf(support), support
 
 
 def pb_a(a, params, model=2):
@@ -52,7 +65,13 @@ def pc(params, model=2):
     amax = params['amax']
     bmin = params['bmin']
     bmax = params['bmax']
-    support = np.arange(params['amax'] + params['bmax'] + 1)
+    support = np.arange(amax + bmax + 1)
+    if model == 1:
+        dist = sum(
+            np.sum(pc_ab(np.arange(amin, amax + 1), b, params, model)[0], axis=1) for b in
+            np.arange(bmin, bmax + 1)) / (amax - amin + 1) / (bmax - bmin + 1)
+        return dist, support
+
     lmbds = np.array([a*p1 + b*p2 for a in np.arange(amin, amax + 1) for b in np.arange(bmin, bmax + 1)])
     all_dists = poisson(lmbds).pmf(np.matrix(support).T)
     weights = np.ones((amax-amin+1)*(bmax-bmin+1))/(amax-amin+1)/(bmax-bmin+1)
@@ -74,27 +93,27 @@ def pb_d(d, params, model=2):
     # p(b|d) = p(b,d)/p(d) = p(a)p(b)\sum_c p(d|c)\sum_a p(c|a,b)/p(d)
     d_dist, d_support = pd(params, model)
 
-    def p_d_c(d, c, params):
-        dist, support = pd_c(c, params)
+    def p_d_c(d, c, params, model):
+        dist, support = pd_c(c, params, model)
         return dist[np.where(support == d)[0][0]]
-    p_d_c_vect = np.array([p_d_c(d, c, params) for c in np.arange((d+1)/2, d+1)])
+    p_d_c_vect = np.array([p_d_c(d, c, params, model) for c in np.arange((d+1)/2, d+1)])
     result = []
     # calculate for every b
     for b in np.arange(params['bmin'], params['bmax']+1):
-        pc_ab_sum_a = np.sum(pc_ab(np.arange(params['amin'], params['amax']+1), b, params)[0], axis=1)
+        pc_ab_sum_a = np.sum(pc_ab(np.arange(params['amin'], params['amax']+1), b, params, model)[0], axis=1)
         result.append(pc_ab_sum_a[(d+1)/2:(d+1)].dot(p_d_c_vect))
     return np.array(result)/sum(result), np.arange(params['bmin'], params['bmax']+1)
 
 
 def pb_ad(a, d, params, model=2):
     # p(b|a,d) = p(a,b,d)/p(a,d) = p(a)p(b)\sum_c p(d|c)p(c|a,b)/p(a)p(b)\sum_b \sum_c p(c|a,b)p(d|c)
-    def p_d_c(d, c, params):
-        dist, support = pd_c(c, params)
-        return dist[np.where(support == d)[0][0]]
-    p_d_c_vect = np.array([p_d_c(d, c, params) for c in np.arange((d+1)/2, d+1)])
+    def p_d_c(dd, cc):
+        dist, support = pd_c(cc, params, model)
+        return dist[np.where(support == dd)[0][0]]
+    p_d_c_vect = np.array([p_d_c(d, c) for c in np.arange((d+1)/2, d+1)])
     result = []
     for b in np.arange(params['bmin'], params['bmax']+1):
-        result.append(pc_ab(a, b, params)[0][(d+1)/2:(d+1)].dot(p_d_c_vect))
+        result.append(pc_ab(a, b, params, model)[0][(d+1)/2:(d+1)].dot(p_d_c_vect))
     return np.array(result)/sum(result), np.arange(params['bmin'], params['bmax']+1)
 
 
