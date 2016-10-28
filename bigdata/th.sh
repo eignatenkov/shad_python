0bc8172782d063e9a8a22c3fbddf888b41943a73
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-10-28 21:57:25 +0300

    set reduce tasks to 1

--- bigdata/th.sh
+++ bigdata/th.sh
@@ -2,6 +2,7 @@
 hdfs dfs -rm -r th/2016-10-27
 
 hadoop jar /opt/hadoop/hadoop-streaming.jar \
+    -D mapreduce.job.reduces=1 \
     -files th_mapper.py,th_reducer.py -input /user/sandello/logs/access.log.2016-10-27 -output th/2016-10-27 \
     -mapper th_mapper.py \
     -reducer th_reducer.py
