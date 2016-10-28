8841eae5912d2786cc11b63fcb2be028f08a4f3e
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-10-28 20:44:27 +0200

    no extra imports

--- bigdata/th_mapper.py
+++ bigdata/th_mapper.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import sys
-from log_tools import get_error_code
+
+
+def get_error_code(log_line):
+    return int(log_line.split('"')[2].split()[0])
 
 
 def main():
     for line in sys.stdin:
         if get_error_code(line) == 200:
-            print 1
+            print '1'
 
 
 if __name__ == '__main__':
