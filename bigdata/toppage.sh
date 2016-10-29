8ee7fe4bfde501a9739fe04438d3af18eab3052f
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-10-29 20:48:29 +0300

    toppages proper output

--- bigdata/toppage.sh
+++ bigdata/toppage.sh
@@ -13,4 +13,5 @@ hadoop jar /opt/hadoop/hadoop-streaming.jar \
     -mapper hadoop_scripts/toppage_mapper.py \
     -reducer hadoop_scripts/toppage_reducer.py
 
-#echo "$DATE,$(hdfs dfs -cat tu/${DATE}/part-00000)" >> "/home/eignatenkov/shad_python/bigdata/top_pages.csv"
+echo "$DATE,$(hdfs dfs -cat toppage/${DATE}/part-00000 | sort -k 2,2nr -k 1,1 | head -10 | awk -F '\t' '{print $1}' | tr '\n' ',')" >> "/home/eignatenkov/shad_python/bigdata/top_pages.csv"
+
