b614d65bb965ee1580fe63208dc00ff5f824e7cd
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-11-16 16:47:26 +0300

    deal with bad lines in session

--- bigdata/hadoop_scripts/session_mapper.py
+++ bigdata/hadoop_scripts/session_mapper.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import sys
+import datetime
 from log_tools import get_error_code, get_page, get_ip, get_time
 
 
 def main():
     for line in sys.stdin:
         if get_error_code(line) == 200:
-            print "{0}\t{1}\t{2}".format(get_ip(line),
-                                         get_time(line),
-                                         get_page(line))
+            try:
+                time = datetime.datetime.strptime(get_time(line), '%d/%b/%Y:%H:%M:%S')
+                print "{0}\t{1}\t{2}".format(get_ip(line),
+                                             get_time(line),
+                                             get_page(line))
+            except ValueError:
+                pass
 
 
 if __name__ == '__main__':
