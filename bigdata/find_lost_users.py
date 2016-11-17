ba9c46b9f4766c0bbc274938e1c1f485a4657e2d
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-11-17 11:42:58 +0100

    rewrite find_lost_users to hdfs

--- bigdata/find_lost_users.py
+++ bigdata/find_lost_users.py
@@ -1,27 +1,32 @@
 #!/shared/anaconda/bin/python
 import datetime
 import argparse
+import subprocess
 from api import iterate_between_dates
 
 
 def count_new_users(date=datetime.date.today()-datetime.timedelta(days=1)):
     old_users = set()
     old_date = date - datetime.timedelta(days=13)
-    if old_date < datetime.date(2016,10,7):
+    if old_date < datetime.date(2016, 10, 7):
         print "{0},{1}".format(date.strftime("%Y-%m-%d"), 0)
         return
-    with open("daily_user/{}.txt".format(old_date.strftime("%Y-%m-%d"))) as f:
-        for line in f:
-            if line.strip() not in old_users:
-                old_users.add(line.strip())
+    h_file = "daily_user/{}/part-00000".format(old_date.strftime("%Y-%m-%d"))
+    cat = subprocess.Popen(["hdfs", "dfs", "-cat", h_file],
+                           stdout=subprocess.PIPE)
+    for line in cat.stdout:
+        if line.strip() not in old_users:
+            old_users.add(line.strip())
 
     start_date = date - datetime.timedelta(days=12)
     end_date = date
 
     for day in iterate_between_dates(start_date, end_date):
-        with open("daily_user/{}.txt".format(day.strftime("%Y-%m-%d"))) as f:
-            for line in f:
-                old_users.discard(line.strip())
+        h_file = "daily_user/{}/part-00000".format(day.strftime("%Y-%m-%d"))
+        cat = subprocess.Popen(["hdfs", "dfs", "-cat", h_file],
+                               stdout=subprocess.PIPE)
+        for line in cat.stdout:
+            old_users.discard(line.strip())
 
     print "{0},{1}".format(date.strftime("%Y-%m-%d"), len(old_users))
 
