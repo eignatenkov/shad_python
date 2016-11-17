2ec40ca87e5948f6460ed7cf52a49ca77dfc0e86
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-11-17 19:43:53 +0300

    recalculate old data for hw2

--- bigdata/hbase/old_profile_hits.sh
+++ bigdata/hbase/old_profile_hits.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PREFIX="/user/sandello/logs/access.log."
 cd ..
-for i in {10..1}
+for i in {41..2}
   do
     DATE=$(date +%Y-%m-%d -d "$i days ago")
     hdfs dfs -rm -r profile_hits/$DATE
